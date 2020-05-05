20fe3b5db01d50716bf5c02c40d9d05b711cba2a
山田直樹 <wakamsha@gmail.com>
2020-05-05 18:08:27 +0900

    :construction: Improve getting-started

--- packages/getting-started/tests/test1.ts
+++ packages/getting-started/tests/test1.ts
@@ -1,12 +1,11 @@
-import 'testcafe';
 import { Selector } from 'testcafe';
 
 fixture('Getting started').page('http://devexpress.github.io/testcafe/example');
 
 test('My first test', async (t: TestController) => {
   await t
-    .typeText('#developer-name', 'wakamsha')
+    .typeText('#developer-name', 'Naoki')
     .click('#submit-button')
     .expect(Selector('#article-header').innerText)
-    .eql('Thank you, wakamsha!');
+    .eql('Thank you, Naoki!');
 });
